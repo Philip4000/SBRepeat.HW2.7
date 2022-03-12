23d9d319d3e580243af6ced0a3d353275a081b41
Philip4000 <6427676@bk.ru>
2022-03-12 18:14:44 +0300

    Initial Commit

---
+++ SBRepeat.HW2.7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SBRepeat.HW2.7
+//
+//  Created by Philip Noskov on 12.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
